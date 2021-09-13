9887ae31b4759217579af1597000e326a69838f8
Alekcey264 <alekseverevkin@gmail.com>
2021-09-13 19:24:30 +0500

    Proverka

--- lab1.c
+++ lab1.c
@@ -1,30 +1,46 @@
 #include <stdio.h>
 
+float max(int a, int b, int c)
+{
+  float h;
+  if ((a>b)&&(a>c))
+  {
+     h = a;
+  }
+  else
+  {
+    if ((b>c)&&(b>a))
+    {
+      h = b;
+    }
+    else
+    {
+      h = c;
+    }
+  }
+  return (h);
+}
+
 int main()
 {
   float a1, a2, a3, i;
   printf("a1 = ");
   scanf("%f", &a1);
-  if ((a1 - (int)a1==0))
-  {
   printf("a2 = ");
   scanf("%f", &a2);
-  if ((a2 - (int)a2==0))
-  {
   printf("a3 = ");
-  scanf(");
-            i=a1 - a3;%f", &a3);
-  if ((a3 - (int)a3==0))
+  scanf("%f", &a3);
+  if (((a1 - (int)a1==0))&&(a2 - (int)a2==0)&&(a3 - (int)a3==0))
   {
     if ((a1+a2+a3)!=0)
     {
-      if ((a1>a2)&&(a1>a3))
+      if (max(a1, a2, a3) == a1)
       {
         if ((int)a1%2==0)
         {
           printf("1\n");
           i=(a1/(a1+a2+a3))*100;
-          printf("asd = %.4f\n ", i);
+          printf("Percent = %.4f\n ", i);
         }
         else
         {
@@ -42,15 +58,13 @@ int main()
           }
         }
       }
-    else
-    {
-      if ((a2>a1)&&(a2>a3))
+      if (max(a1, a2, a3) == a2)
       {
         if ((int)a2%2==0)
         {
           printf("2\n");
           i=(a2/(a1+a2+a3))*100;
-          printf("asd = %.4f\n", i);
+          printf("Percent = %.4f\n", i);
         }
         else
         {
@@ -68,13 +82,13 @@ int main()
           }
         }
       }
-      else
+      if (max(a1, a2, a3) == a3)
       {
         if ((int)a3%2==0)
         {
           printf("3\n");
           i = (a3/(a1+a2+a3))*100;
-          printf("asd = %.4f\n", i);
+          printf("Percent = %.4f\n", i);
         }
         else
         {
@@ -92,7 +106,6 @@ int main()
           }
         }
       }
-    }
   }
   else
   {
@@ -103,16 +116,5 @@ int main()
   {
     printf("Для дробных чисел не существует понятий 'Четность' и 'Нечетность', пожалуйста, введите целые числа\n");
   }
-  }
-  else
-  {
-    printf("Для дробных чисел не существует понятий 'Четность' и 'Нечетность', пожалуйста, введите целые числа\n");
-  }
-  }
-  else
-  {
-    printf("Для дробных чисел не существует понятий 'Четность' и 'Нечетность', пожалуйста, введите целые числа\n");
-  }
-  printf("sdfsdfsd");
 return(0);
 }
