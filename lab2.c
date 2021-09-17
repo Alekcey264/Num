e089f441bce9f0c6d747db90367e250b3a7ca849
Alekcey264 <alekseverevkin@gmail.com>
2021-09-17 16:06:59 +0500

    Отредактировано для сдачи по красоте

--- lab2.c
+++ lab2.c
@@ -3,36 +3,35 @@
 
 int main()
 {
-  float x, y, z, ct;
-  printf("x= ");
-  scanf("%f", &x);
-  float pi = 3.14;
-  if ((x!=(int)x)&&(pow(2, x)>=-1)&&(pow(2, x)<=1))
-  {
-    ct = 1 / tan(pi * x);
-    y = ct + acos(pow(2, x));
-    printf("y= %f \n", y);
-  }
-  else
-  {
-    printf("Переменная x не удовлетворяет области определения функции y = ctg(pi*x) + arccos(2^x)\n");
-  }
-  if (x<0)
-  {
-    z = -1;
-  }
-  else
-  {
-    if (x>0)
+    float x, y, z, ct;
+    printf("x= ");
+    scanf("%f", &x);
+    float pi = M_PI;
+    if ((x != (int)x) && (pow(2, x) >= -1) && (pow(2, x) <= 1))
     {
-      z = 1;
+        ct = 1 / tan(pi * x);
+        y = ct + acos(pow(2, x));
+        printf("y= %f \n", y);
     }
     else
     {
-      z = 0;
+        printf("Переменная x не удовлетворяет области определения функции y = ctg(pi*x) + arccos(2^x)\n");
     }
-  }
-  printf("z= %f \n", z);
-  printf("asd");
-  return 0;
+    if (x < 0)
+    {
+        z = -1;
+    }
+    else
+    {
+        if (x > 0)
+        {
+            z = 1;
+        }
+        else
+        {
+            z = 0;
+        }
+    }
+    printf("z= %f \n", z);
+    return 0;
 }
