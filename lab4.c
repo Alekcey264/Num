4e3df07f427d8438abb5ac75b8eaf909e9230078
Alekcey264 <alekseverevkin@gmail.com>
2021-10-08 20:48:13 +0500

    бьу

--- lab4.c
+++ lab4.c
@@ -2,60 +2,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double f1(double x)
+long double f1(long double x)
 {
-  double y = sqrt(x + 1) - sqrt(x) - 0.5;
-  return (y);
+    long double y = sqrt(x + 1) - sqrt(x) - 0.5;
+    return (y);
 }
 
-double f2(double l)
+long double f2(long double l)
 {
-  double j = -l - 1 / l;
-  double e = exp(j);
-  return (e);
+    long double j = -l - 1 / l;
+    long double e = exp(j);
+    return (e);
 }
 
-double inh(double t, double y, double u)
-{
-  double inter = 0;
-  double l = (y - t) / u;
-  for (int i = 0; i < (u - 1); i++)
-    {
-      double k = (t + i * l) + (l / 2);
-      inter = inter + f1(k);
-    }
-    return (inter * l);
-}
 
-double inh2(double t, double y, double u)
+long double inh(long double t, long double y, long double u, int p)
 {
-  double inter = 0;
-  double l = (y - t) / u;
-  for (int i = 0; i < (u - 1); i++)
-    {
-      double k = (t + i * l) + (l / 2);
-      inter = inter + f2(k);
-    }
+    long double inter = 0;
+    long double l = (y - t) / u;
+    for (int i = 0; i < (u - 1); i++)
+      {
+          long double k = (t + i * l) + (l / 2);
+          if (p == 1)
+            inter = inter + f1(k);
+          else if (p == 2)
+            inter = inter + f2(k);
+      }
     return (inter * l);
 }
 
 int main()
 {
-    double n, a1, b1, a2, b2, del, integral, integral2;
-    double eps = 0.1;
+    long double n, a1, b1, a2, b2, del, integral, integral2;
+    long double eps = 0.0001;
     a1 = 0;
     b1 = 1;
     a2 = 1;
     b2 = 2;
-    n = 1000;
+    n = 100;
+    //scanf("%Lf\n", &eps);
     do
     {
-    integral = inh(a1, b1, n) + inh2(a2, b2, n);
-    integral2 = inh(a1, b1, (2 * n)) + inh2(a2, b2, (2 * n));
-    del = abs(integral2 - integral) / 3;
-    n = 2 * n;
+        integral = inh(a1, b1, n, 1) * 1000 + inh(a2, b2, n, 2) * 1000;
+        integral2 = inh(a1, b1, (2 * n), 1) * 1000 + inh(a2, b2, (2 * n), 2) * 1000;
+        del = abs((integral2 - integral) * 10000000);
+        del = del / 10000000000;
+        del = del / 3;
+        n = 2 * n;
     }
     while (del >= eps);
-    printf("%f\n", integral);
+    printf("%.9Lf\n", integral / 1000);
     return 0;
 }
