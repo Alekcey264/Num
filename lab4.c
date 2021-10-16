21001671d15718cd5d145e3da52d0770c31f2841
Alekcey264 <alekseverevkin@gmail.com>
2021-10-16 10:36:55 +0500

    Готово

--- lab4.c
+++ lab4.c
@@ -2,54 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long double f1(long double x)
+double inh(double t, double y, double u, int p)
 {
-    long double y = sqrt(x + 1) - sqrt(x) - 0.5;
-    return (y);
-}
-
-long double f2(long double l)
-{
-    long double j = -l - 1 / l;
-    long double e = exp(j);
-    return (e);
-}
-
-
-long double inh(long double t, long double y, long double u, int p)
-{
-    long double inter = 0;
-    long double l = (y - t) / u;
+    double inter = 0;
+    double l = (y - t) / u;
     for (int i = 0; i < (u - 1); i++)
       {
-          long double k = (t + i * l) + (l / 2);
+          double k = (t + i * l) + (l / 2);
           if (p == 1)
-            inter = inter + f1(k);
+            inter = inter + (sqrt(k + 1) - sqrt(k) - 0.5);
           else if (p == 2)
-            inter = inter + f2(k);
+            inter = inter + exp(-k - 1 / k);
       }
     return (inter * l);
 }
 
+
 int main()
 {
-    long double n, a1, b1, a2, b2, del, integral, integral2;
-    long double eps = 0.0001;
+    double n, a1, b1, b2, del, integral, integral2;
+    double eps;
     a1 = 0;
     b1 = 1;
-    a2 = 1;
     b2 = 2;
     n = 100;
+    scanf("%lf", &eps);
     do
     {
-        integral = inh(a1, b1, n, 1) * 1000 + inh(a2, b2, n, 2) * 1000;
-        integral2 = inh(a1, b1, (2 * n), 1) * 1000 + inh(a2, b2, (2 * n), 2) * 1000;
-        del = abs((integral2 - integral) * 10000000);
-        del = del / 10000000000;
-        del = del / 3;
+        integral = inh(a1, b1, n, 1) + inh(b1, b2, n, 2);
+        integral2 = inh(a1, b1, (2 * n), 1) + inh(b1, b2, (2 * n), 2);
+        del = fabs((integral2 - integral)) / 3;
         n = 2 * n;
     }
     while (del >= eps);
-    printf("%.9Lf\n", integral / 1000);
+    printf("%.9f\n", integral);
     return 0;
 }
